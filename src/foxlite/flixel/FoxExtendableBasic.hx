093b461f6937a8d9897abc8e543ea45db0d00aff
mayo78 <75394229+mayo78@users.noreply.github.com>
2026-09-11 13:46:58 -0400

    we arent base fnf just cause we have polymod :/ :/ :/ evil :/

--- src/foxlite/flixel/FoxExtendableBasic.hx
+++ src/foxlite/flixel/FoxExtendableBasic.hx
@@ -1,8 +1,3 @@
 package foxlite.flixel;
 
-#if polymod
-import funkin.modding.base.ScriptedFlxBasic;
-class FoxExtendableBasic extends ScriptedFlxBasic {}
-#else
 typedef FoxExtendableBasic = flixel.FlxBasic;
-#end
--- src/foxlite/funkin/FoxExtendableSprite.hx
+++ src/foxlite/funkin/FoxExtendableSprite.hx
@@ -1,10 +1,3 @@
 package foxlite.funkin;
 
-#if polymod
-import funkin.modding.base.ScriptedFlxSprite;
-class FoxExtendableSprite extends ScriptedFlxSprite {}
-#elseif cne
-typedef FoxExtendableSprite = funkin.backend.FunkinSprite;
-#else
 typedef FoxExtendableSprite = flixel.FlxSprite;
-#end
