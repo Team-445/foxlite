7dfbd5cc8fd5b5345e14fdb78c6b0af323984bf6
mayo78 <75394229+mayo78@users.noreply.github.com>
2026-09-23 13:52:18 -0400

    Be consistant with foxlite formatting

--- src/foxlite/FoxLog.hx
+++ src/foxlite/FoxLog.hx
@@ -1,14 +1,11 @@
 package foxlite;
 
-class FoxLog 
-{
-	public static function log(origin:String, message:String)
-	{
+class FoxLog {
+	public static function log(origin:String, message:String):Void {
 		trace('[FoxLite > $origin]: $message');
 	}
 
-	public static function warning(origin:String, message:String)
-	{
+	public static function warning(origin:String, message:String):Void {
 		trace('[FoxLite > $origin] WARNING: $message');
 	}
 }
